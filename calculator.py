d8ba24ceddd6351cec7b063ac4b67b9f7022c9e4
VarunNurav <147610855+VarunNurav@users.noreply.github.com>
2023-10-29 15:52:22 +0530

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,41 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
+while True:
+    print("Options:")
+    print("Enter '1' for addition")
+    print("Enter '2' for subtraction")
+    print("Enter '3' for multiplication")
+    print("Enter '4' for division")
+    print("Enter '5' to quit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '5':
+        break
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        elif choice == '2':
+            print("Result:", subtract(num1, num2))
+        elif choice == '3':
+            print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input. Please enter a valid choice.")
